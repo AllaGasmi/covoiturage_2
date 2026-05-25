e7c794632dcffa8939d9f4900ec06a43debf9697
Mayoumaoun <oumaymaounissi2004@gmail.com>
2026-05-25 15:49:07 +0100

    updated trips and bookings controller to use auth

--- src/trips/trips.controller.ts
+++ src/trips/trips.controller.ts
@@ -7,37 +7,40 @@ import {
   Param,
   Body,
   ParseIntPipe,
+  UseGuards,
+  Req,
 } from '@nestjs/common';
 import { TripsService } from './trips.service';
 import { CreateTripDto } from './dto/create-trip.dto';
 import { UpdateTripDto } from './dto/update-trip.dto';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 
 @Controller('trips')
-// @UseGuards(JwtAuthGuard)  // à activer qd auth mise en place
+@UseGuards(JwtAuthGuard)  
 export class TripsController {
   constructor(private readonly tripsService: TripsService) {}
 
   @Post()
-  create(@Body() dto: CreateTripDto) {
-    const driverId = 1; // ← remplace par req.user.id avec auth
+  create(@Body() dto: CreateTripDto,@Req() request: any) {
+    const driverId = request.user.id;
     return this.tripsService.createTrip(driverId, dto);
   }
 
   @Put(':id')
-  update(@Param('id', ParseIntPipe) id: number, @Body() dto: UpdateTripDto) {
-    const driverId = 1;
+  update(@Param('id', ParseIntPipe) id: number, @Body() dto: UpdateTripDto,@Req() request: any) {
+    const driverId = request.user.id;
     return this.tripsService.updateTrip(id, driverId, dto);
   }
 
   @Delete(':id')
-  cancel(@Param('id', ParseIntPipe) id: number) {
-    const driverId = 1;
+  cancel(@Param('id', ParseIntPipe) id: number,@Req() request: any) {
+    const driverId = request.user.id;
     return this.tripsService.cancelTrip(id, driverId);
   }
 
   @Get('mine')
-  getMyTrips() {
-    const driverId = 1;
+  getMyTrips(@Req() request: any) {
+    const driverId = request.user.id;
     return this.tripsService.getMyTrips(driverId);
   }
 }
